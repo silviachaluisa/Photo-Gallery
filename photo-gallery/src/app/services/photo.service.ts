18e3084709677da082d104e15af8160ef7edd333
silviachaluisa <veronica-14-1998@hotmail.com>
2024-10-18 14:48:22 -0500

    Tercer Commit

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -8,31 +8,67 @@ import { Preferences } from '@capacitor/preferences';
   providedIn: 'root'
 })
 export class PhotoService {
-  public photos: UserPhoto[]=[]
+  public photos: UserPhoto[] = [];
 
-  constructor() { }
-  
-  public async addNewToGallery(){
-    //Take a photo
-    const capturedPhoto=await Camera.getPhoto({
-      resultType:CameraResultType.Uri,
-      source:CameraSource.Camera,
-      quality:100
+  // Función para convertir una imagen en base64
+  private async readAsBase64(photo: Photo): Promise<string> {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  // Función para convertir un Blob en base64
+  private convertBlobToBase64 = (blob: Blob): Promise<string | ArrayBuffer | null> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+  };
+
+  // Función para guardar la imagen en el sistema de archivos
+  private async savePicture(photo: Photo): Promise<UserPhoto> {
+    // Convertir la foto a base64
+    const base64Data = await this.readAsBase64(photo);
+
+    // Escribir el archivo en el directorio de datos
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
     });
 
-     //Añadir foto a la galeria
-    this.photos.unshift({
-      filepath:"soon..",
-      webviewPath: capturedPhoto.webPath!
+    // Devolver la ruta del archivo guardado
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  constructor() {}
+
+  // Añadir nueva foto a la galería
+  public async addNewToGallery() {
+    // Tomar una foto
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
     });
-    
+
+    // Guardar la imagen y añadirla a la colección de fotos
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
 }
 
-export interface UserPhoto{
+// Definición de la interfaz UserPhoto
+export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
-
 }
-
-
